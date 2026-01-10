cf28031582cded8bbec23196da4e480640572385
Aditya <adinair.in@gmail.com>
2026-01-09 19:45:57 -0600

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/GrayMatra-Landing2.0/',  // ← IMPORTANT: Must match your repo name exactly!
+  base: '/graymatra-landing2.0/',  // ← IMPORTANT: Must match your repo name exactly!
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
